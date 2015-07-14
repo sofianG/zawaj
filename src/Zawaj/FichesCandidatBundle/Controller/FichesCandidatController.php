bcf86a8b6cbf3e03de85155833eed7884a9a1eb0
fckhribgatous <sofian.guendaf@gmail.com>
2015-07-14 14:36:33 +0200

    création des routes, du controleur, et de la vue (recus, inscrits, archives) - intégration de bootstrap - création du fichier FichesCandidat.css - paramétrage de l'Assétic

--- src/Zawaj/FichesCandidatBundle/Controller/FichesCandidatController.php
+++ src/Zawaj/FichesCandidatBundle/Controller/FichesCandidatController.php
@@ -10,4 +10,49 @@ class FichesCandidatController extends Controller
     {
         return $this->render('ZawajFichesCandidatBundle::index.html.twig');
     }
+
+
+    public function fichesRecusAction($typeSexe, $id)
+    {
+        if ($typeSexe == 'Homme'){
+        return $this->render('ZawajFichesCandidatBundle::fichesRecus.html.twig', array(
+            'typeSexe' => $typeSexe
+            ));
+        }
+        else {
+            return $this->render('ZawajFichesCandidatBundle::fichesRecus.html.twig', array(
+                'typeSexe' => $typeSexe
+            ));
+        }
+    }
+
+
+    public function fichesInscritsAction($typeSexe, $id)
+    {
+        if ($typeSexe == 'Homme'){
+            return $this->render('ZawajFichesCandidatBundle::fichesInscrits.html.twig', array(
+                'typeSexe' => $typeSexe
+            ));
+        }
+        else {
+            return $this->render('ZawajFichesCandidatBundle::fichesInscrits.html.twig', array(
+                'typeSexe' => $typeSexe
+            ));
+        }
+    }
+
+
+    public function fichesArchivesAction($typeSexe, $id)
+    {
+        if ($typeSexe == 'Homme'){
+            return $this->render('ZawajFichesCandidatBundle::fichesArchives.html.twig', array(
+                'typeSexe' => $typeSexe
+            ));
+        }
+        else {
+            return $this->render('ZawajFichesCandidatBundle::fichesArchives.html.twig', array(
+                'typeSexe' => $typeSexe
+            ));
+        }
+    }
 }
